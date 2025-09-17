70b2b92deba2b3cd209ce62be6aab0c4d7a8134b
shantanumankar26 <mankarshantanu30@gmail.com>
2025-09-17 14:15:41 +0530

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -42,9 +42,10 @@ INSERT INTO silver.crm_cust_info (
 	cst_key,
 	cst_firstname,
 	cst_lastname,
-	cst_material_status,
+	cst_marital_status,
 	cst_gndr,
-	cst_create_date) 
+	dwh_create_date
+	) 
 
 SELECT
 cst_id,
@@ -72,6 +73,95 @@ WHERE cst_id IS NOT NULL
 SET @end_time = GETDATE();
 		PRINT '>> Load Duration:' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + 'seconds';
 		PRINT '--------------------------------------------------';
+
+-- LOADING silver.crm_prd_info
+SET @start_time = GETDATE();
+PRINT'>> Truncating Table: silver.crm_prd_info';
+TRUNCATE TABLE silver.crm_prd_info;
+PRINT 'Inserting Data Into: silver.crm_prd_info' ;
+INSERT INTO silver.crm_prd_info (
+	prd_id,
+	prd_key,
+	cat_id,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+	)
+	SELECT
+		prd_id,
+		REPLACE(SUBSTRING(prd_key,1,5),'-','_') AS cat_id,
+		SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
+		prd_nm,
+		ISNULL(prd_cost, 0) AS prd_cost,
+		CASE
+			WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+			WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+			WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+			WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+			ELSE 'n/a'
+		END	AS prd_line,
+		CAST(prd_start_dt AS DATE) AS prd_start_dt,
+		CAST(
+			LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1
+			AS DATE
+			) AS prd_end_dt
+		FROM bronze.crm_prd_info;
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration:' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + 'seconds';
+		PRINT '--------------------------------------------------';
+
+-- LOADING silver.crm_sales_details
+SET @start_time = GETDATE();
+PRINT'>> Truncating Table: silver.crm_sales_details';
+TRUNCATE TABLE silver.crm_sales_details;
+PRINT 'Inserting Data Into: silver.crm_sales_details' ;
+INSERT INTO silver.crm_sales_details (
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	sls_order_dt,
+	sls_ship_dt,
+	sls_due_dt,
+	sls_sales,
+	sls_quantity,
+	sls_price
+	)
+	SELECT	
+		sls_ord_num,
+		sls_prd_key,
+		sls_cust_id,
+		CASE 
+			WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+			ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+		END AS sls_order_dt,
+		CASE 
+			WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+			ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+		END AS sls_ship_dt,
+		CASE 
+			WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
+			ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+		END AS sls_due_dt,
+		CASE 
+			WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
+				THEN sls_quantity * ABS(sls_price)
+			ELSE sls_sales
+		END AS sls_sales,
+		sls_quantity,
+		CASE 
+			WHEN sls_price IS NULL OR sls_price <= 0
+				THEN sls_sales / NULLIF(sls_quantity,0)
+				ELSE sls_price
+		END AS sls_price
+		FROM bronze.crm_sales_details;
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration:' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + 'seconds';
+		PRINT '--------------------------------------------------';
+
+
+
 
 -- LOADING silver.erp_cust_az12
 SET @start_time = GETDATE();
