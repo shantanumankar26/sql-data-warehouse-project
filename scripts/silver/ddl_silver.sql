42a8d652ec5f15563abea04e66fc1d5083b89fbb
shantanumankar26 <mankarshantanu30@gmail.com>
2025-09-17 13:49:55 +0530

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -8,18 +8,16 @@ Script Purpose:
   Run this script to re-define the DDL structure of 'bronze' tables
 ==============================================================================
 */
-IF OBJECT_ID ('silver.crm_prd_info', 'U') IS NOT NULL
-	DROP TABLE silver.crm_prd_info;
+IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE silver.crm_cust_info;
 GO
-CREATE TABLE silver.crm_prd_info (
-	prd_id INT,
-	cat_id NVARCHAR(50),
-	prd_key NVARCHAR(50),
-	prd_nm NVARCHAR(50),
-	prd_cost INT,
-	prd_line NVARCHAR(50),
-	prd_start_dt DATE,
-	prd_end_dt DATE,
+CREATE TABLE silver.crm_cust_info (
+	cst_id,
+	cst_key,
+	cst_firstname,
+	cst_lastname,
+	cst_marital_status,
+	cst_gndr,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 	);
 GO
@@ -30,6 +28,7 @@ GO
 CREATE TABLE silver.crm_prd_info (
 prd_id INT,
 prd_key NVARCHAR(50),
+cat_id NVARCHAR(50),
 prd_nm NVARCHAR(50),
 prd_cost INT,
 prd_line NVARCHAR(50),
